931c6ff1c2524cb80411e381d2f9e4c20253ce98
WinstonMathamba <winstonmathamba@gmail.com>
2016-08-05 14:53:20 -0400

    Hopefully a final commit to homework Day 4

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,18 @@
 public class Main {
     public static void main(String [] args) {
 
-        Puppy puppy1 = new Puppy("Poodle", "white ", true);
+        Puppy puppy1 = new Puppy("Poodle", "white", true, "sweet");
         puppy1.weight = 25;
 
-        Puppy puppy2 = new Puppy("Chow Chow", "brown ", false);
+        Puppy puppy2 = new Puppy("Chow Chow", "brown", false, "mean");
         puppy2.weight = 40;
 
 
 
+
         System.out.println(puppy1);
+        System.out.println(puppy2);
+        puppy2.likesWalk =true;
         System.out.println(puppy2);
 
 
--- src/Puppy.java
+++ src/Puppy.java
@@ -4,25 +4,39 @@
 public class Puppy {
     String breed;
     String color;
+    String temperment;
     double weight;
     boolean likesWalk;
 
 
-    // Wrote a class titled Puppy with five properties
+    // Wrote a class titled Puppy with four properties
 
-    public Puppy(String breed, String color, boolean likesWalk) {
+    public Puppy(String breed, String color, boolean likesWalk, String temperment) {
         this.breed = breed;
         this.color = color;
         this.likesWalk = likesWalk;
+        this.temperment = temperment;
 
-        //This is constructor for puppy with two arguments for breed and color
     }
+        //This is constructor for puppy with two arguments for breed and color
+
 
     public String toString() {
-        return "This " +
-                this.color + this.breed + " weighs " + this.weight + "lbs!";
+        String response =  "This " + this.temperment + " " + this.color + " " + this.breed + " weighs "
+                + this.weight + "lbs";
+
+        if(likesWalk) {
+            response = response + ", and also likes going for long walks";
+        } else {
+            response = response + ", and hates to go for walks :(";
+
+        }
+        return response;
+
     }
 
 }
+
+
 
 

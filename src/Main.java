1659e1364f80c18fa62e251ee69f6399aa17e9c4
WinstonMathamba <winstonmathamba@gmail.com>
2016-08-04 20:58:01 -0400

    Another commit for Day 4 Homework

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String [] args) {
 
-        Puppy puppy1 = new Puppy("Poodle", "White ", true);
+        Puppy puppy1 = new Puppy("Poodle", "white ", true);
         puppy1.weight = 25;
 
         Puppy puppy2 = new Puppy("Chow Chow", "brown ", false);
@@ -13,6 +13,7 @@ public class Main {
 
 
         System.out.println(puppy1);
+        System.out.println(puppy2);
 
     }
 
